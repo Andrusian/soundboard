2c33d1cf287dce33a35c775f01855b904abb2c8b
Andrusian <spent.fuel@gmail.com>
2023-04-22 22:45:11 -0400

    First version with all buttons wired. This is for DFPlayer Pro but none of the audio functions are implemented.
    This also fully deals with conversion between Teensy C code and Wiring (Arduino) code.
    There is some strange bug in the libraries: hopefully this is now resolved.
    
    You need to pick the kind of USB usage in the Arduino menu (e.g. Keyboard). Very little
    code would need to be added to make this a USB keyboard.

--- soundboard.ino
+++ soundboard.ino
@@ -1,8 +1,5 @@
-#include <SoftwareSerial.h>
-
-#include <DFRobotDFPlayerMini.h>
-
-
+//#include <DFRobot_DF1201S.h>           // FOR DFPlayer PRO VERSION
+// is using the mini, much of the DFPlayer commands has to change
 //
 //  It is assumed that this will be used with an external amp.
 //
@@ -28,7 +25,7 @@
 //             Lat     PC1 o     o o   o PF1   Lat    Mom o PA4 o PA0  LEDON
 //             Lat     PC2 o     o o   o PF2          Mom o PA5 o PA1  LEDRUN
 //             Lat     PC3 o     o o   o PF3          Mom o PA6 o PA2
-//             Mom     PC4 o  o  o o   o PF4   Mom    Mom o PA7 o PA3  DKmini STATUS
+//             Mom     PC4 o  o  o o   o PF4   Mom    Mom o PA7 o PA3  
 //             Mom     PC5 o           o PF5   Mom      
 //             Mom     PC6 o           o PF6   Mom       o ALE
 //             Mom     PC7 o---o-o-o---o PF7   Mom
@@ -40,7 +37,6 @@
 //  Mom - momentary button 
 //  Lat - Latched button  
 //  All buttons have pull up resistors enabled (so they are wired to ground).
-//  ALL DKplayer GND PINS MUST BE GROUNDED!
 //
 
 
@@ -49,88 +45,60 @@
 #define LEDRUNON  (PORTA |= (1<<1))
 #define LEDRUNOFF (PORTA &= ~(1<<1))
 
-#define HIGH 1
-#define LOW 0                   
+                
 #define LOOPDELAY 1000           // time delay of main loop ms
 #define INTSCALER 10
 #define BUSYPIN 31
 
 
-SoftwareSerial mySoftwareSerial(4, 5); // RX, TX
-#define DFPserial_RX 4  // DFP RX pin 2 connect to Arduino TX pin 5
-#define DFPserial_TX 5  // DFP TX pin 3 connect to Arduino RX pin 4
-
-DFRobotDFPlayerMini myDFPlayer;
+// SoftwareSerial mySoftwareSerial(4, 5); // RX, TX
+// #define DFPserial_RX 4  // DFP RX pin 2 connect to Arduino TX pin 5
+// #define DFPserial_TX 5  // DFP TX pin 3 connect to Arduino RX pin 4
 
 void setup() {
-  // put your setup code here, to run once:
-// standard bootup...
-
-
 
   // initialize command queue
 
   // initialize IO pins...
 
-  (DDRA |= (1<<0));      // PORT A has the 2 LEDs
-  (DDRA |= (1<<1));
+  DDRA = 0b00000011;       // bottom 2 pins are outputs
+  PORTA = 0b11111100;      // rest have pull-up resistors
 
   DDRC =  0b00000000;     // port C is all inputs
-  PORTC = 0b11111111;    // port C uses pull up resistors
+  PORTC = 0b11111111;     // port C uses pull up resistors
   DDRB =  0b00000000;     // port B is all inputs
-  PORTB = 0b11111111;    // port B uses pull up resistors
-  DDRA&=~(1<<3);    // pin A3 is input for DFplayer status
+  PORTB = 0b11111111;     // port B uses pull up resistors
+  DDRF =  0b00000000;     // port F is all inputs
+  PORTF = 0b11111111;     // port B uses pull up resistors
   
   debugLights();
 
   delay(1000);
 
-  mySoftwareSerial.begin(9600);
-  Serial.begin(9600);
+   // Keyboard.println(F("soundboard hello!"));
+
+
   
-  Serial.println();
-  Serial.println(F("DFRobot DFPlayer Mini Demo"));
-  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-
-;
-  myDFPlayer.begin(mySoftwareSerial);
-  delay(200);
-  if (0) {  //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
-    Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert the SD card!"));
-    while(true);
-  }
-  Serial.println(F("DFPlayer Mini online."));
-
-
-  //----Set volume----
-  myDFPlayer.volume(20);  //Set volume value (0~30).
-
-  delay(3000);
-
-
-  Serial.println(F("play 1"));
-  myDFPlayer.play(1); delay(31000);
-  myDFPlayer.play(2); delay(31000);
-  myDFPlayer.play(3); delay(31000);
-  myDFPlayer.play(4); delay(31000);
-  myDFPlayer.play(5); delay(31000);
-  myDFPlayer.play(6); delay(31000);
-  myDFPlayer.play(7); delay(31000);
-     
-
+  //  DF1201Serial.begin(115200);
+  // while(!DF1201S.begin(DF1201SSerial)){
+    //Serial.println("Init failed, please check the wire connection!");
+    //delay(1000);
+  //}
 
 }
 
+//--------------------------------------------------------------------------------------------
 
+unsigned int getDFStatus() {
+
+  return !(PORTA & (1<<3));
+}
 
 //---------------------------------------------------------------------------------------------
 
 void loop() {
   int modifier=0;
-  int value=0;
-  unsigned int selected=0;
+  unsigned int selected=256;
 
   delay(1000);
  
@@ -138,64 +106,131 @@ void loop() {
   // momentary button values. this vastly modifies the number
   // of sounds that can be played.
 
+  unsigned char A=PINA;
   unsigned char C=PINC;
   unsigned char B=PINB;
+  unsigned char F=PINF;
+
+  // find position of latched buttons first...
 
   unsigned char b128=!(C & (1<<3));
   unsigned char b64=!(C & (1<<2));
   unsigned char b32=!(C & (1<<1));
   unsigned char b16=!(C & (1<<0));
+
+  unsigned char bclr=!(F & (1<<1));
+  unsigned char brun=!(F & (1<<0));
   
   if (b16) {
-    modifier+=16;
+    modifier+=16; // red latched button
   }
-  if (b32) {
+  if (b32) {     // blue right latched button
     modifier+=32;
   }
-  if (b64) {
+  if (b64) {    // blue left latched button
     modifier+=64;
   }
-  if (b128) {
+  if (b128) {   // white latched button
     modifier+=128;
   }
+
+  // buttons are numbered top left
+  // to bottom right - these are momentaries
+
+  unsigned char b0=!(B & (1<<3));   // top yellow bank
+  unsigned char b1=!(B & (1<<2));
+  unsigned char b2=!(B & (1<<1));
+  unsigned char b3=!(B & (1<<0));
   
-  unsigned char b12=!(B & (1<<0));
-  unsigned char b13=!(B & (1<<1));
-  unsigned char b14=!(B & (1<<2));
-  unsigned char b15=!(B & (1<<3));
-    
-  // put your main code here, to run repeatedly:
+  unsigned char b4=!(F & (1<<6));   // blue bank
+  unsigned char b5=!(F & (1<<7));
+  unsigned char b6=!(F & (1<<4));
+  unsigned char b7=!(F & (1<<5));
+  
+  unsigned char b8=!(C & (1<<6));   // black bank
+  unsigned char b9=!(C & (1<<7));
+  unsigned char b10=!(C & (1<<4));
+  unsigned char b11=!(C & (1<<5));
+   
+  unsigned char b12=!(F & (1<<7));   // yellow bank
+  unsigned char b13=!(F & (1<<6));
+  unsigned char b14=!(F & (1<<4));  
+  unsigned char b15=!(F & (1<<5));
 
-  // we'll read the buttons one at a time
-  // this means if two buttons are pressed, the
-  // higher number button overrides the earlier one
+  Serial.print (A,BIN);
+  Serial.print (" ");
+  Serial.print (B,BIN);
+  Serial.print (" ");
+  Serial.print (C,BIN);
+  Serial.print (" ");
+  Serial.print (F,BIN);
+  Serial.println (" ");
+  Serial.println("");
+  
+  // lower number pressed button
+  // overrides higher one
 
-  if (b12) {
+  if (b0) {
+    selected=0;
+  }
+  else if (b1) {
+    selected=1;
+  }
+  else if (b2) {
+    selected=2;
+  }
+  else if (b3) {
+    selected=3;
+  }
+  else if (b4) {
+    selected=4;
+  }
+  else if (b5) {
+    selected=5;
+  }
+  else if (b6) {
+    selected=6;
+  }
+  else if (b7) {
+    selected=7;
+  }
+  else if (b8) {
+    selected=8;
+  }
+  else if (b9) {
+    selected=9;
+  }
+  else if (b10) {
+    selected=10;
+  }
+  else if (b11) {
+    selected=11;
+  }  
+  else if (b12) {
     selected=12;
   }
-  if (b13) {
+  else if (b13) {
     selected=13;
   }
-  if (b14) {
+  else if (b14) {
     selected=14;
   }
-  if (b15) {
+  else if (b15) {
     selected=15;
   }
 
-  value=selected+modifier;
+  unsigned int value=selected+modifier;
 
   // was a momentary button pressed?
 
-  // phex(C);
-  // phex(B);
   
-  if (1) {
-
-    // for debugging, blink some lights
-
-    blinkLights(modifier,selected);
-  }
+//  if (selected!=256) {
+//    Serial.println(F("selected:"));
+//    Serial.print(selected,DEC);
+//    Serial.print(" ");
+//    Serial.print(value,DEC);
+//    Serial.println("");
+//  }
   
 }
 
